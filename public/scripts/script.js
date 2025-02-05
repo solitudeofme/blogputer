cb09a4e8ac9d100d27ecc4f0630c9618a00a88f3
Nilou <desnillight@gmail.com>
2025-02-05 16:44:04 +0330

    making menu for mobile

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,47 +1,28 @@
-const themeToggleBtn = document.querySelector(".theme-toggle-btn");
-themeToggleBtn.addEventListener("click", () => {
-  if (localStorage.theme === "dark") {
-    document.documentElement.classList.remove("dark");
-    localStorage.setItem("theme", "light");
-  } else {
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("theme", "dark");
-  }
+"use strict";
+const themeToggleBtns = document.querySelectorAll(".theme-toggle-btn");
+const navBarBtn = document.querySelector(".nav-bar-btn");
+const overlayMenu = document.querySelector(".overlay-menu");
+const overlay = document.querySelector(".overlay");
+
+navBarBtn.addEventListener("click", () => {
+  overlay.classList.toggle("hidden");
+  overlayMenu.classList.remove("-left-64");
+  overlayMenu.classList.add("left-0");
 });
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/*.{html,js}"],
-  darkMode: "selector",
-  theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: "1rem",
-      },
-      cursor: {
-        custom: "url(../../images/cursor.png)",
-      },
-      colors: {
-        "light-primary": "#9D8C7A",
-        "dark-primary": "#413A31",
-      },
-      fontFamily: {
-        silkscreen: "Silkscreen",
-        fredoka: "Fredoka",
-      },
-    },
-    screens: {
-      xs: "480px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-    },
-  },
-  plugins: [
-    function ({ addVariant }) {
-      addVariant("child", "& > *");
-      addVariant("child-hover", "& > *:hover");
-    },
-  ],
-};
+overlay.addEventListener("click", () => {
+  overlay.classList.toggle("hidden");
+  overlayMenu.classList.remove("left-0");
+  overlayMenu.classList.add("-left-64");
+});
+
+themeToggleBtns.forEach((themeToggleBtn) => {
+  themeToggleBtn.addEventListener("click", () => {
+    if (localStorage.theme === "dark") {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  });
+});
